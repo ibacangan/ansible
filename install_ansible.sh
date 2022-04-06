f678f003f9817382e31607381b69fe63ddf5bfca
i.bacangan@gmail.com <i.bacangan@gmail.com>
2022-04-06 20:07:22 +0800

    some improvement

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y \
-    python3 \
-    python3-pip \
-    git
+if [[ "$(which ansible)" == "" ]]; then
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install -y \
+        python3 \
+        python3-pip \
+        git
 
-pip3 install ansible
+    pip3 install ansible
+
+    export PATH=$PATH:~/.local/bin
+fi
 
 [[ -d /tmp/ansible ]] || git clone https://github.com/ibacangan/ansible.git /tmp/ansible
 
 cd /tmp/ansible
-
-export PATH=$PATH:~/.local/bin
 
 ansible-galaxy install -r requirements.yaml
 ansible-playbook -i localhost install.yaml
