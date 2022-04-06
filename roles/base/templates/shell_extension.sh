ed78438d1bb6beb8ef77d029a681e5d8dfcd1974
i.bacangan@gmail.com <i.bacangan@gmail.com>
2022-04-06 22:54:58 +0800

    move loading alias to refresh env script

--- roles/base/templates/shell_extension.sh
+++ roles/base/templates/shell_extension.sh
@@ -6,8 +6,4 @@ e LOCAL_BIN=~/.local/bin
 e BASE_PATH=$PATH:$LOCAL_BIN:{{ script_directory }}
 e PATH=$BASE_PATH
 
-for alias_file in {{ alias_directory }}/*; do
-  source $alias_file
-done
-
 . refresh_env
