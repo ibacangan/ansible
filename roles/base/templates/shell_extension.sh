b93151572771f282b17ebc61e25dc048509dd57f
ibacangan <i.bacangan@gmail.com>
2022-04-23 15:12:49 +0800

    update installation scripts

--- roles/base/templates/shell_extension.sh
+++ roles/base/templates/shell_extension.sh
@@ -4,7 +4,9 @@ source $ZSH/oh-my-zsh.sh
 alias a=alias
 a e=export
 
-e TF_PLUGIN_CACHE_DIR=/workspace/tmp/terraform_cache
+e TF_PLUGIN_CACHE_DIR={{ system_directory }}/hashicorp
+mkdir -p $TF_PLUGIN_CACHE_DIR
+
 e LOCAL_BIN=~/.local/bin
 e BASE_PATH=$PATH:$LOCAL_BIN:{{ script_directory }}
 e PATH=$BASE_PATH
